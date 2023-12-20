d303c5bba763e2a91294e6ce43c5eadfae411d69
Dipendra <dipendrabhandari0000@gmail.com>
2023-12-20 18:06:09 +0545

    first commit

---
+++ prime.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main()
+{
+   
+    int a, i,c;
+    printf("Enter any no");
+    scanf("%d", &a);
+    for(i=1;i<=a;i++)
+{
+     if(a%==0)
+     c++;
+}
+  if (c==2)
+  
+    printf("The no is prime");
+  else
+  printf("The no is composite");
+
+
+}
